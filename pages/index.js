febdfda82b1e9bf2284e513ed411801e5bbc32bf
Tomek Rozalski <github@tomaszrozalski.pl>
2022-04-18 21:23:25 +0200

    remove caching

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,18 @@ export default function Index({ time }) {
       <h1>SSR Caching with Next.js</h1>
       <time dateTime={time}>{time}</time>
     </main>
-  )
+  );
 }
 
 export async function getServerSideProps({ req, res }) {
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=10, stale-while-revalidate=59'
-  )
+  // res.setHeader(
+  //   'Cache-Control',
+  //   'public, s-maxage=10, stale-while-revalidate=59'
+  // )
 
   return {
     props: {
       time: new Date().toISOString(),
     },
-  }
+  };
 }
